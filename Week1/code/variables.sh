ab0f4860e296ac7fed17002d78cf6a024404344a
ChalitaCh <chalita.chomkatekaew20@imperial.ac.uk>
2021-10-15 09:27:25 +0100

    Fixed script and final submission

--- Week1/code/variables.sh
+++ Week1/code/variables.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
-# Shows the use of variables
-MyVar='some string'
-echo 'the current values of the variable is' $MyVar
-echo 'Please enter a new string'
-read MyVar
-echo 'the current values of the variable is' $MyVar
 
-## Reading multiple values
+## Illustrates the use of variables
+
+# Special variables
+
+echo "This script was called with $# parameters"
+echo "The script's name is $0"
+echo "The arguments are $@"
+echo "The first argument is $1"
+echo "The second argument is $2"
+
+# Assigned Variables; Explicit declaration
+MY_VAR='some string'
+echo 'the current value of the variable is:' $MY_VAR
+echo
+echo 'Pleas enter a new string'
+read MY_VAR
+echo
+echo 'the current value of the variable is:' $MY_VAR
+echo
+
+## Assigned Variables; Reading (mulitple values) from user input:
 echo 'Enter two numbers separated by space(s)'
 read a b
 echo 'you entered' $a 'and' $b '. Their sum is:'
-mysum=$(expr $a + $b)
-echo $mysum
+MY_SUM=$(expr $a + $b)
+echo $MY_SUM
