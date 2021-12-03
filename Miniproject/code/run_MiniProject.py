a98872b1753d67ed66530af03ee5603afdff50c2
ChalitaCh <92088550+ChalitaCh@users.noreply.github.com>
2021-12-03 16:53:49 +0000

    Update run_MiniProject.py

--- Miniproject/code/run_MiniProject.py
+++ Miniproject/code/run_MiniProject.py
@@ -12,13 +12,13 @@ import subprocess
 ## code
 
 print('Cleaning the data')
-#subprocess.Popen("Rscript --verbose DataCleaning.R > ../results/log_dataclean.log 2> ../results/log_dataclean.err", shell=True).wait()
+subprocess.Popen("Rscript --verbose DataCleaning.R > ../results/log_dataclean.log 2> ../results/log_dataclean.err", shell=True).wait()
 
 print('Fitting the model and optimising the starting values')
-#subprocess.Popen("Rscript --verbose Model_fittings.R > ../results/log_modelfitting.log 2> ../results/log_modelfitting.err", shell=True).wait()
+subprocess.Popen("Rscript --verbose Model_fittings.R > ../results/log_modelfitting.log 2> ../results/log_modelfitting.err", shell=True).wait()
 
 print('Analysing the data')
-#subprocess.Popen("Rscript --verbose Model_selection_analysis.R > ../results/log_model_analysis.log 2> ../results/log_modelanalysis.err", shell=True).wait()
+subprocess.Popen("Rscript --verbose Model_selection_analysis.R > ../results/log_model_analysis.log 2> ../results/log_modelanalysis.err", shell=True).wait()
 
 print('Plotting the beautiful graphs!')
 subprocess.Popen("Rscript --verbose Data_visualisation.R > ../results/log_datavis.log 2> ../results/log_datavis.err", shell=True).wait()
